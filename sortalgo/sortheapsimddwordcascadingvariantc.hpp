51bbcd89953b57bfec24c31d80d9f9bd3655ad6d
Piotr Tarsa <piotr.tarsa@gmail.com>
2014-05-25 19:52:12 +0200

    Added attribution

--- sortalgo/sortheapsimddwordcascadingvariantc.hpp
+++ sortalgo/sortheapsimddwordcascadingvariantc.hpp
@@ -109,6 +109,9 @@ namespace tarsa {
             return _mm_min_epu32(a, b);
         }
 
+        /*
+         * based on: http://stackoverflow.com/a/23593786/492749
+         */
         template<typename ItemType, bool Signed, bool Ascending>
         ssize_t leaderIndex(ItemType const * const a) {
             __v8si vec = _mm256_load_si256((__m256i *) a);
